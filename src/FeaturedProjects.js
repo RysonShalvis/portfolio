3b67894507a08ccff9622f6d10450c80dd809cd2
Ryson Shalvis <rysonshalvis@Rysons-MacBook-Pro.local>
2021-02-15 17:16:51 -0700

    test

--- src/FeaturedProjects.js
+++ src/FeaturedProjects.js
@@ -27,7 +27,7 @@ class FeaturedProjects extends Component {
                         </div>
                         <h2 className="more-info"><svg className="computer-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M7 17H2a2 2 0 0 1-2-2V2C0 .9.9 0 2 0h16a2 2 0 0 1 2 2v13a2 2 0 0 1-2 2h-5l4 2v1H3v-1l4-2zM2 2v11h16V2H2z"></path></svg>API AND SERVERS</h2>
                         <h2 className="more-info"><svg className="computer-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M.7 9.3l4.8-4.8 1.4 1.42L2.84 10l4.07 4.07-1.41 1.42L0 10l.7-.7zm18.6 1.4l.7-.7-5.49-5.49-1.4 1.42L17.16 10l-4.07 4.07 1.41 1.42 4.78-4.78z"></path></svg>REACT.JS, CSS, AND JSX</h2>
-                        <Link to="/yelp-api">
+                        <Link to="/portfolio/yelp-api">
                             <button className="project-details btn">View Details</button>
                         </Link>
                     </div>
