905b83fa29c6e492cbf70a20f9b93e1355bc4ccd
RysonShalvis <rysonshalvis@gmail.com>
2021-06-25 14:51:22 -0600

    adding mobile responsivienss

--- src/Header.js
+++ src/Header.js
@@ -15,14 +15,24 @@ class Header extends Component {
 
     render() {
         return (
-            <div className="header-ctn">
-                <h1 className="header-name">Ryson Shalvis</h1>
-                <p>This is a work in progress, not all features will be available!</p>
-                <div className="nav-link-ctn">
-                    <a href="./"><h2 className="nav-link">Home</h2></a>
-                    <Link to="/"><h2 onClick={this.onProjects} className="nav-link">Projects</h2></Link>
-                    <h2 onClick={this.onContact} className="nav-link">Contact</h2>
+            <div>
+                <div className="header-ctn-mobile mobile">
+                    <h1>Ryson Shalvis</h1>
+                    <div className="hamburger-icon-ctn">
+                        <div className="hamburger-icon"></div>
+                        <div className="hamburger-icon"></div>
+                        <div className="hamburger-icon"></div>
+                    </div>
                 </div>
+                <div className="header-ctn desktop">
+                    <h1 className="header-name">Ryson Shalvis</h1>
+                    <p>This is a work in progress, not all features will be available!</p>
+                    <div className="nav-link-ctn">
+                        <a href="./"><h2 className="nav-link">Home</h2></a>
+                        <Link to="/"><h2 onClick={this.onProjects} className="nav-link">Projects</h2></Link>
+                        <h2 onClick={this.onContact} className="nav-link">Contact</h2>
+                    </div>
+            </div>
             </div>
         );
     }
