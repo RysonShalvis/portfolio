43d60d8a3ccb8e0903203ccf7869b2d7abed0f45
RysonShalvis <rysonshalvis@gmail.com>
2021-07-04 00:08:50 -0600

    fix left diagonal and more responsiveness

--- src/FeaturedProjects.js
+++ src/FeaturedProjects.js
@@ -22,8 +22,8 @@ class FeaturedProjects extends Component {
                         <h1 className="featured-project-name title-right-desktop">Yelp API</h1>
                         <h2 className="featured-project-summary yelp-summary">Search for different businesses in any location with reviews/ratings using an API from the Yelp Servers.</h2>
                         <div className="lightbulb-ctn">
-                            <img src={lightbulb} alt="" className="lightbulb"/>
-                            <h2 className="more-info">FULLY FUNCTIONAL</h2>
+                            
+                            <h2 className="more-info"><img src={lightbulb} alt="" className="lightbulb"/> FULLY FUNCTIONAL</h2>
                         </div>
                         <h2 className="more-info"><svg className="computer-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M7 17H2a2 2 0 0 1-2-2V2C0 .9.9 0 2 0h16a2 2 0 0 1 2 2v13a2 2 0 0 1-2 2h-5l4 2v1H3v-1l4-2zM2 2v11h16V2H2z"></path></svg>API AND SERVERS</h2>
                         <h2 className="more-info"><svg className="computer-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M.7 9.3l4.8-4.8 1.4 1.42L2.84 10l4.07 4.07-1.41 1.42L0 10l.7-.7zm18.6 1.4l.7-.7-5.49-5.49-1.4 1.42L17.16 10l-4.07 4.07 1.41 1.42 4.78-4.78z"></path></svg>REACT.JS, CSS, AND JSX</h2>
@@ -45,8 +45,8 @@ class FeaturedProjects extends Component {
                         <h1 className="featured-project-name title-left-desktop">Invision Clone</h1>
                         <h2 className="featured-project-summary">A perfect clone of Invision App's home website.</h2>
                         <div className="lightbulb-ctn">
-                            <img src={lightbulb} alt="" className="lightbulb"/>
-                            <h2 className="more-info">EXACT REPLICA</h2>
+                           
+                            <h2 className="more-info"> <img src={lightbulb} alt="" className="lightbulb"/>EXACT REPLICA</h2>
                         </div>
                         <h2 className="more-info"><svg className="computer-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M7 17H2a2 2 0 0 1-2-2V2C0 .9.9 0 2 0h16a2 2 0 0 1 2 2v13a2 2 0 0 1-2 2h-5l4 2v1H3v-1l4-2zM2 2v11h16V2H2z"></path></svg>BUILT FROM SCRATCH</h2>
                         <h2 className="more-info"><svg className="computer-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M.7 9.3l4.8-4.8 1.4 1.42L2.84 10l4.07 4.07-1.41 1.42L0 10l.7-.7zm18.6 1.4l.7-.7-5.49-5.49-1.4 1.42L17.16 10l-4.07 4.07 1.41 1.42 4.78-4.78z"></path></svg> JAVASCRIPT, CSS, AND HTML</h2>
@@ -77,17 +77,17 @@ class FeaturedProjects extends Component {
 
                 <div className="third-underline under-line underlines"></div>
                 <h1 className="featured-project-name mobile-title" >Trick Creator</h1>
-                <div className="featured-project-ctn">
-                    <div className="img-ctn">
-                        <img src={trickImage1} alt="The Trick Creator Website" className="trick-creator"/>
-                        <img src={trickImage2} alt="The Trick Creator Website" className="trick-creator"/>
+                <div id="trick-creator--img-ctn" className="featured-project-ctn">
+                    <div  className="img-ctn">
+                        <img src={trickImage1} alt="The Trick Creator Website"  className="trick-creator"/>
+                        <img src={trickImage2} alt="The Trick Creator Website" id="trick-creator-img" className="trick-creator"/>
                     </div>
                     <div className="featured-project-summary-ctn">
                         <h1 className="featured-project-name title-right-desktop">Trick Creator</h1>
                         <h2 className="featured-project-summary trick-creator-summary">A simple game that allows you to combine tricks to make more and more complex tricks.</h2>
                         <div className="lightbulb-ctn">
-                            <img src={lightbulb} alt="" className="lightbulb"/>
-                            <h2 className="more-info">OWN CONCEPT</h2>
+                            
+                            <h2 className="more-info"><img src={lightbulb} alt="" className="lightbulb"/> OWN CONCEPT</h2>
                         </div>
                         <h2 className="more-info"><svg className="computer-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M7 17H2a2 2 0 0 1-2-2V2C0 .9.9 0 2 0h16a2 2 0 0 1 2 2v13a2 2 0 0 1-2 2h-5l4 2v1H3v-1l4-2zM2 2v11h16V2H2z"></path></svg>BUILT FROM SCRATCH</h2>
                         <h2 className="more-info"><svg className="computer-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M.7 9.3l4.8-4.8 1.4 1.42L2.84 10l4.07 4.07-1.41 1.42L0 10l.7-.7zm18.6 1.4l.7-.7-5.49-5.49-1.4 1.42L17.16 10l-4.07 4.07 1.41 1.42 4.78-4.78z"></path></svg>REACT, JAVASCRIPT, CSS, AND JSX</h2>
