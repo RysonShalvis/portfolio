9a9ae49fa708def8a7419cebf71285ebcffc7046
RysonShalvis <rysonshalvis@gmail.com>
2021-07-05 12:50:20 -0600

    add full page trick creator

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import Header from './Header'
 import Home from './Home';
 import Contact from './Contact';
 import { HashRouter, Route } from 'react-router-dom';
-import NotAvailable from './NotAvailable';
+//import NotAvailable from './NotAvailable';
 import YelpApi from './YelpApi';
 import fullpageInvision from './fullpageInvision';
+import FullPageTrickCreator from './FullPageTrickCreator';
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route exact path="/portfolio/yelp-api" component={YelpApi} />
         <Route exact path="/portfolio/invision-clone" component={fullpageInvision} />
-        <Route exact path="/portfolio/trick-creator" component={NotAvailable} />
+        <Route exact path="/portfolio/trick-creator" component={FullPageTrickCreator} />
         <Contact />
       </HashRouter>
     </div>
